a94fbf1e791105f0a70e329fb4dfe1ab8d7a49eb
Muhammad Hussain <148378219+22muhahuss@users.noreply.github.com>
2024-12-08 11:46:26 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Get the modal
 var modal = document.getElementById('login');
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
